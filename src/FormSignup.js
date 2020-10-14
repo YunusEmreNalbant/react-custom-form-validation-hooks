342a2802e2617707d6beb54b51c3d291fa638f43
YunusEmreNalbant <nalbantyunusemre@hotmail.com>
2020-10-14 14:52:29 +0300

    Displaying Error Messages

--- src/FormSignup.js
+++ src/FormSignup.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import useForm from './useForm'
 import validate from './validateInfo';
-
+import './Form.css'
 function FormSignup() {
 
-    const {handleChange,values,handleSubmit} = useForm(validate); 
+    const {handleChange,values,handleSubmit,errors} = useForm(validate); 
 
     return (
         <div>
@@ -16,20 +16,21 @@ function FormSignup() {
                             Username
                         </label>
                         <input id="username" type="text" name='username' className="form-input" value={values.username} onChange={handleChange} placeholder="Enter your username"/>
-
+                        {errors.username && <p>{errors.username}</p>}
                     </div>
                     <div className="form-inputs">
                         <label htmlFor="email" className="form-label">
                             Email
                         </label>
                         <input id="email" type="email" name='email' className="form-input"  value={values.email} onChange={handleChange} placeholder="Enter your email"/>
-
+                        {errors.email && <p>{errors.email}</p>}
                     </div>
                     <div className="form-inputs">
                         <label htmlFor="password" className="form-label">
                         Password
                         </label>
                         <input id="password" type="password" name='password' className="form-input"  value={values.password} onChange={handleChange} placeholder="Enter your password"/>
+                        {errors.password && <p>{errors.password}</p>}
 
                     </div>
                     <div className="form-inputs">
@@ -37,6 +38,7 @@ function FormSignup() {
                             Confrim Password
                         </label>
                         <input id="password2" type="password" name='password2' className="form-input"  value={values.password2} onChange={handleChange} placeholder="Enter your password2"/>
+                        {errors.password2 && <p>{errors.password2}</p>}
 
                     </div>
                     <button className="form-input-ntn" type="submit">Sign up</button>
